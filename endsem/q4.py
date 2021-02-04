d0886ddd7f6b9bdc3ec2c9b1f395ac518a4a6416
Jabir <mjabir.t@niser.ac.in>
2021-02-04 13:04:18 +0530

    Add files via upload

---
+++ endsem/q4.py
@@ -0,0 +1,16 @@
+from functionlib import *
+from math import *
+
+# Q.4
+dy  = lambda u, y, x : u
+ddy = lambda u, y, x : -9.8
+a, b = 0, 5
+y, x = shooting_method(ddy, dy, 2, 0, 45, 0.02)
+
+print('the final velocity = 47.89999999999995')
+
+## output ##
+# Guess the 1st value of slope: 40
+# Guess the 2nd value of slope: 50
+# The obtained solution for z is : 47.89999999999995
+# the final velocity = 47.89999999999995 
